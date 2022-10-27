dcdbb8cdaec47e008f3dc4610931ac0d660b5a2a
DEIOSHA SPARKS <DEIOSHASPARKS13@GMAIL.COM>
2022-10-26 18:25:49 -0700

    new updates

--- js/app.js
+++ js/app.js
@@ -1,5 +1,7 @@
 'use strict';
 
+let finalScore = 0;
+
 console.log('Hello World');
 
 let visitorName = prompt('What is your name?');
@@ -12,6 +14,7 @@ let questionOneGuess = prompt('Do I like red velvet cake?').toUpperCase();
 if(questionOneGuess === 'YES' || questionOneGuess === 'Y') {
   // console.log('The answer is correct.)
   alert('Correct!');
+  finalScore++;
 } else if(questionOneGuess === 'NO' || questionOneGuess === 'N') {
 // console.log('The answer is incorrect')
   alert('Incorrect, I love red velvet cake.');
@@ -22,6 +25,7 @@ let questionTwoGuess = prompt('Is pink my favorite color?').toUpperCase();
 if(questionTwoGuess === 'YES' || questionTwoGuess === 'Y') {
   // console.log('The answer is correct.)
   alert('Good job!!');
+  finalScore++;
 } else if(questionTwoGuess === 'NO' || questionTwoGuess === 'N') {
 // console.log('The answer is incorrect')
   alert('Incorrect! Maybe you will answer correctly next time!');
@@ -35,6 +39,7 @@ if(questionThreeGuess === 'YES' || questionThreeGuess === 'Y') {
 } else if(questionThreeGuess === 'NO' || questionThreeGuess === 'N') {
   // console.log('The answer is correct.)
   alert('You are correct. I am orginally from STL');
+  finalScore++;
 }
 
 let questionFourGuess = prompt('Am I 23 years old?').toUpperCase();
@@ -42,6 +47,7 @@ let questionFourGuess = prompt('Am I 23 years old?').toUpperCase();
 if(questionFourGuess === 'YES' || questionFourGuess === 'Y') {
   // console.log('The answer is correct.)
   alert('Great job!');
+  finalScore++;
 } else if(questionFourGuess === 'NO' ||
 questionFourGuess === 'N') {
 // console.log('The answer is incorrect')
@@ -56,7 +62,51 @@ if(questionFiveGuess === 'YES' || questionFiveGuess === 'Y') {
 } else if(questionFiveGuess === 'NO' || questionFiveGuess === 'N') {
   // console.log('The answer is correct.)
   alert('You are correct! I am a Taurus.');
+  finalScore++;
+}
+
+let myNum = 5;
+let guess = 0;
+let totalGuesses = 4;
+let correct = false;
+
+while (guess < totalGuesses && ! correct){
+  let userGuess = prompt('Guess a number I am thinking');
+  if (userGuess < myNum) {
+    alert('Too low! Try again.');
+    guess ++;
+    console.log(guess);
+  } else if(userGuess > myNum) {
+    alert('Too high! Try again.');
+    guess ++;
+  } else {
+    alert('Correct!!');
+    finalScore++;
+    correct = true;
+  }
+}
+
+let guesstwo = 0;
+let totalGuessesTwo = 6;
+let correctTwo = false;
+
+let colors = ['Blue','Red', 'Pink','Orange', 'Black', 'purple'];
+
+while (guesstwo < totalGuessesTwo && ! correctTwo){
+  let userGuessTwo = prompt('Guess a color');
+  if (userGuessTwo < colors) {
+    alert('Guess Again!');
+    guesstwo ++;
+  } else {
+    alert('You are correct!');
+    finalScore++;
+    correctTwo = true;
+  } if (totalGuessesTwo > guesstwo) {
+    alert('the correct answers are Red, Pink ,Orange, Black, and purple');
+  }
 }
 
 
-alert(`Thank you for playing ${visitorName}. Thanks for coming to my site!`);
+
+
+alert(`Thank you for playing ${visitorName}. Thanks for coming to my site! You got ${finalScore} out of 7 correct.`);
